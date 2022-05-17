e133832672679bbca2568a0c9aea5a725a8296a5
ZinebGH <sdafzineb1s2@gmail.com>
2022-05-17 23:49:23 +0200

    Modif & début  tests pour visualiser les résultats

--- WordPressAPI.py
+++ WordPressAPI.py
@@ -1,33 +1,41 @@
-import json
 import requests
-from requests.auth import HTTPBasicAuth
 
 
 class WordPressAPI:
     def __init__(self, user, password):
         self.user = user
         self.password = password
-        self.API = "https://webeniz.wpcomstaging.com/2022/05/16/hello-world/"
+        self.API = "https://etu2cas.wpcomstaging.com"
+        self.token = None
 
     def connect(self):
-        response = requests.request(
-            'POST', self.API, auth=HTTPBasicAuth(self.user, self.password))
+        response = requests.post(
+            self.API + "/wp-json/jwt-auth/v1/token", data={'username': self.user, 'password': self.password})
         if response.status_code == 200:
             print("Succesful connection with API.")
-            data = response.json()
+            self.token = response.json()["token"]
+            print(self.token)
         elif response.status_code == 404:
             print("Unable to reach URL.")
         else:
-            print(f"There is a {response.status_code} error with your request")
+            print(
+                f"There is a {response.status_code} error with your request {response.json()}")
 
     def get(self, endpoint):
-        response = requests.get(endpoint)
+        headers = {'Authorization': 'Bearer {}'.format(self.token)}
+        print(self.token)
+        response = requests.get(endpoint, headers=headers, verify=False)
+
         if response.status_code == 200:
             print("Succesful connection with API")
             return response.json()
+        elif response.status_code == 403:
+            self.connect()
+            self.get(endpoint)
         elif response.status_code == 404:
             print("Unable to reach URL.")
             return None
         else:
-            print(f"There is a {response.status_code} error with your request")
+            print(
+                f"There is a {response.status_code} error with your request {response.json()}")
             return None
--- auth.py
+++ auth.py
@@ -8,11 +8,21 @@ def authentification(user, password):
     wpa = WordPressAPI(user, password)
     wpa.connect()
 
-    if print(wpa.get()) != None:
+    response = wpa.get(
+        "https://etu2cas.wpcomstaging.com//wp-json/jwt-auth/v1/token/validate")
+
+    # response = wpa.get(
+    #     "https://etu2cas.wpcomstaging.com/wp-json/wp/v2/users/me")
+
+    if response != None:
         return True
     else:
         return False
 
 
 if __name__ == '__main__':
-   print(" ")
+    #authentification("ttt", "ttttTTTT1111")
+    if authentification("Test", "etudedecas2022"):
+        print("Authentification accepted")
+    else:
+        print("Authentification refused")
